71a2330b9f8fcfbc881de0bad9f93a22e3af5195
vinicius-araujo777 <lucaslemoss608@gmail.com>
2026-05-18 11:10:44 -0300

    Iniciando página de Login

---
+++ auth/login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Barbearia</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="POST">
+        <label for="username">Nome:</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <label for="password">Senha:</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Entrar</button>
+</body>
+</html>
